1764b31b4f47ddee6edc12c2bfb36cb1586350a1
HencyPithadiya <wrteamhency@gmail.com>
2026-01-20 17:50:59 +0530

    doc open load tiem reduce

--- application/assets/js/theme.js
+++ application/assets/js/theme.js
@@ -11,7 +11,7 @@
 	"use strict";
 
 // Preloader
-$(window).on('load', function () {
+$(document).ready(function () {
 	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
 	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(333);
